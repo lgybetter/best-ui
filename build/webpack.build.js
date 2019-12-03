c92ba551de6a9fca209215739706fb27360fa299
lgybetter <437675103@qq.com>
2019-12-03 15:52:55 +0800

    :art: adjust struct

--- build/webpack.build.js
+++ build/webpack.build.js
@@ -11,6 +11,7 @@ const config = merge(baseConf, {
     path: resolve(process.cwd(), './lib'),
     publicPath: '/dist/',
     filename: 'best-ui.js',
+    library: 'BEST',
     libraryTarget: 'umd'
   },
   optimization: {
